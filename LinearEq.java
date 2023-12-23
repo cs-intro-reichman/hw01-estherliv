3efe57aebb7a195c6a470fb427828e0d122aa025
Esther Livingstone <esther.livingstone@post.runi.ac.il>
2023-12-23 17:16:59 +0200

    Update LinearEq.java

--- LinearEq.java
+++ LinearEq.java
@@ -9,11 +9,8 @@ public class LinearEq {
         double a = Double.parseDouble(args[0]);
         double b = Double.parseDouble(args[1]);
         double c = Double.parseDouble(args[2]);
-
-        double z = c - b;
-        double x = z / a;
-
         System.out.println(a + " * x + " + b + " = " + c);
+        double x = (c - b) / a;
         System.out.println("x = " + x);
     }
 }
